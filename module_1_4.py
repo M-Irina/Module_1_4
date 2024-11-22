06a5a28a384846af604a42dc67f09acb431e3db9
M-Irina <irina24mad@gmail.com>
2024-11-23 00:18:27 +0900

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+my_string = input("Напишите номер строки ")
+print(type(my_string))
+current_numbers = 2
+age = current_numbers
+print("Номер Вашей строки ", my_string)
+print("Номер состоит из ", age, "символов." )
+print('напишите номер строки '.upper())
+print('напишите номер строки '.lower())
+print('Напишите номер строки '.replace(' ',''))
+print('Напишите номер строки'.replace('Напишите номер строки','Напишите'))
+print('Напишите номер строки'.replace('Напишите номер строки','строки'))
